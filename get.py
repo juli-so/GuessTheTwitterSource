515646d35ed6cdb25ef09478f2cd235c95fde349
Ethan Eirinberg <ethan@anymealapp.com>
2014-07-23 16:39:49 -0500

    updated

--- get.py
+++ get.py
@@ -47,31 +47,31 @@ producthunt = twitter.get_user_timeline(screen_name='producthunt', include_rts=N
 for idx, tweet in enumerate(producthunt):
     if idx == 0:
     	tweet1ph = tweet['text'] + "\n"
-    	p = tweet1ph.find("http:")
+    	p = tweet1ph.find("htt")
     	tweet1ph = tweet1ph[:p]
     if idx == 1:
     	tweet2ph = tweet['text'] + "\n"
-    	p = tweet2ph.find("http:")
+    	p = tweet2ph.find("htt")
     	tweet2ph = tweet2ph[:p]
     if idx == 2:
     	tweet3ph = tweet['text'] + "\n"
-    	p = tweet3ph.find("http:")
+    	p = tweet3ph.find("htt")
     	tweet3ph = tweet3ph[:p]
     if idx == 3:
     	tweet4ph = tweet['text'] + "\n"
-    	p = tweet4ph.find("http:")
+    	p = tweet4ph.find("htt")
     	tweet4ph = tweet4ph[:p]
     if idx == 4:
     	tweet5ph = tweet['text'] + "\n"
-    	p = tweet5ph.find("http")
+    	p = tweet5ph.find("htt")
     	tweet5ph = tweet5ph[:p]
     if idx == 5:
     	tweet6ph = tweet['text'] + "\n"
-    	p = tweet6ph.find("http")
+    	p = tweet6ph.find("htt")
     	tweet6ph = tweet6ph[:p]
     if idx == 6:
     	tweet7ph = tweet['text'] + "\n"
-    	p = tweet7ph.find("http")
+    	p = tweet7ph.find("htt")
     	tweet7ph = tweet7ph[:p]
 
 
@@ -79,60 +79,60 @@ onion = twitter.get_user_timeline(screen_name='theonion', include_rts=None, coun
 for idx, tweet in enumerate(onion):
     if idx == 0:
         tweet1to = tweet['text'] + "\n"
-        p = tweet1to.find("http:")
+        p = tweet1to.find("htt")
         tweet1to = tweet1to[:p]
     if idx == 1:
         tweet2to = tweet['text'] + "\n"
-        p = tweet2to.find("http:")
+        p = tweet2to.find("htt")
         tweet2to = tweet2to[:p]
     if idx == 2:
         tweet3to = tweet['text'] + "\n"
-        p = tweet3to.find("http:")
+        p = tweet3to.find("htt")
         tweet3to = tweet3to[:p]
     if idx == 3:
         tweet4to = tweet['text'] + "\n"
-        p = tweet4to.find("http:")
+        p = tweet4to.find("htt")
         tweet4to = tweet4to[:p]
     if idx == 4:
     	tweet5to = tweet['text'] + "\n"
-    	p = tweet5to.find("http")
+    	p = tweet5to.find("htt")
     	tweet5to = tweet5to[:p]
     if idx == 5:
     	tweet6to = tweet['text'] + "\n"
-    	p = tweet6to.find("http")
+    	p = tweet6to.find("htt")
     	tweet6to = tweet6to[:p]
     if idx == 6:
     	tweet7to = tweet['text'] + "\n"
-    	p = tweet7to.find("http")
+    	p = tweet7to.find("htt")
     	tweet7to = tweet7to[:p]
 
 hackernews = twitter.get_user_timeline(screen_name='newsycombinator', include_rts=None, count=7)
 for idx, tweet in enumerate(hackernews):
     if idx == 0:
         tweet1hn = tweet['text'] + "\n"
-        p = tweet1hn.find("http:")
+        p = tweet1hn.find("htt")
         tweet1hn = tweet1hn[:p]
     if idx == 1:
         tweet2hn = tweet['text'] + "\n"
-        p = tweet2hn.find("http:")
+        p = tweet2hn.find("htt")
         tweet2hn = tweet2hn[:p]
     if idx == 2:
         tweet3hn = tweet['text'] + "\n"
-        p = tweet3hn.find("http:")
+        p = tweet3hn.find("htt")
         tweet3hn = tweet3hn[:p]
     if idx == 3:
         tweet4hn = tweet['text'] + "\n"
-        p = tweet4hn.find("http:")
+        p = tweet4hn.find("htt")
         tweet4hn = tweet4hn[:p]
     if idx == 4:
     	tweet5hn = tweet['text'] + "\n"
-    	p = tweet5hn.find("http")
+    	p = tweet5hn.find("htt")
     	tweet5hn = tweet5hn[:p]
     if idx == 5:
     	tweet6hn = tweet['text'] + "\n"
-    	p = tweet6hn.find("http")
+    	p = tweet6hn.find("htt")
     	tweet6hn = tweet6hn[:p]
     if idx == 6:
     	tweet7hn = tweet['text'] + "\n"
-    	p = tweet7hn.find("http")
+    	p = tweet7hn.find("htt")
     	tweet7hn = tweet7hn[:p]
