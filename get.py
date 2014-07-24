25a0c36853a3a1900c5b6333b11f67a288533551
Ethan Eirinberg <ethan@anymealapp.com>
2014-07-24 07:55:48 -0500

    more tweets

--- get.py
+++ get.py
@@ -16,7 +16,7 @@ randomt = ""
 
 # Requires Authentication as of Twitter API v1.1
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-techcrunch = twitter.get_user_timeline(screen_name='techcrunch', count=20, include_rts=False)
+techcrunch = twitter.get_user_timeline(screen_name='techcrunch', count=40, include_rts=False)
 for tweet in techcrunch:
     tweet = tweet['text'] + "\n"
     p = tweet.find("htt")
@@ -24,7 +24,7 @@ for tweet in techcrunch:
     tweet = tweet.encode('utf-8')
     arraytc.append(tweet)
 
-producthunt = twitter.get_user_timeline(screen_name='producthunt', count=20, include_rts=False)
+producthunt = twitter.get_user_timeline(screen_name='producthunt', count=40, include_rts=False)
 for tweet in producthunt:
     tweet = tweet['text'] + "\n"
     p = tweet.find("htt")
@@ -32,8 +32,7 @@ for tweet in producthunt:
     tweet = tweet.encode('utf-8')
     arrayph.append(tweet)
 
-
-onion = twitter.get_user_timeline(screen_name='theonion', count=20, include_rts=False)
+onion = twitter.get_user_timeline(screen_name='theonion', count=40, include_rts=False)
 for tweet in onion:
     tweet = tweet['text'] + "\n"
     p = tweet.find("htt")
@@ -41,7 +40,7 @@ for tweet in onion:
     tweet = tweet.encode('utf-8')
     arrayto.append(tweet)
 
-hackernews = twitter.get_user_timeline(screen_name='newsycombinator', count=20, include_rts=False)
+hackernews = twitter.get_user_timeline(screen_name='newsycombinator', count=40, include_rts=False)
 for tweet in hackernews:
     tweet = tweet['text'] + "\n"
     p = tweet.find("htt")
